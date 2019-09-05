4ce45e6af53a730a9e5aabc620e9142a4e8f1c20
Jryke <rykerson@gmail.com>
2019-09-05 13:56:49 +0700

    070104

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 // 070102
 const items = require("./data.js")
+const calculations = require("./calc.js")
 
 items.forEach(item => console.log(item))
+
+// 070104
+console.log(calculations.add(items.map(item => item.price)))
+console.log(calculations.subtract(items.map(item => item.price)))
